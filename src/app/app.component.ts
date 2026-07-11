8d6b266c52c8dafb0c712552bb118e3376fe4d62
Fabio815 <fabiomotamarques815@gmail.com>
2026-07-11 16:51:35 -0300

    Criando uma calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { olaMundoComponent } from './olamundo/olamundo.component';
+import { MinhapaginaComponent } from './minhapagina/minhapagina.component'
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, olaMundoComponent],
+  imports: [olaMundoComponent, MinhapaginaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
