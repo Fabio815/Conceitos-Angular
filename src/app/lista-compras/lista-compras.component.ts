bed100b13fe43a7117feb4d7f2586737245a2d9e
Fabio815 <fabiomotamarques815@gmail.com>
2026-07-16 20:07:22 -0300

    Criei uma classe de item com seus atributos e adicionei esses itens uma lista, exibindo-os com um for

--- src/app/lista-compras/lista-compras.component.ts
+++ src/app/lista-compras/lista-compras.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Item } from './item';
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-lista-compras',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './lista-compras.component.html',
   styleUrl: './lista-compras.component.scss'
 })
 export class ListaComprasComponent {
   item: string = '';
+  itens: Item[] = [];
 
   adicionarItem() {
-    console.log("Item recebido", this.item);
+    let item = new Item();
+    item.id = this.itens.length + 1;
+    item.nome = this.item;
+
+    this.itens.push(item);
+    console.table(this.itens);
+
+    this.item = '';
   }
 }
