950f0f05c894227c57b8f3698ea24f89b2d0efc6
Fabio815 <fabiomotamarques815@gmail.com>
2026-07-20 19:39:24 -0300

    Començando a fazer CRUD de clientes usando o Angular material para a interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
-/*import { RouterOutlet } from '@angular/router';
-import { olaMundoComponent } from './olamundo/olamundo.component';
+import { RouterOutlet } from '@angular/router';
+/*import { olaMundoComponent } from './olamundo/olamundo.component';
 import { MinhapaginaComponent } from './minhapagina/minhapagina.component'*/
 //import { CalculadoraComponent } from './calculadora/calculadora.component';
 import { ListaComprasComponent } from './lista-compras/lista-compras.component'
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [ListaComprasComponent],
+  imports: [RouterOutlet, MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
