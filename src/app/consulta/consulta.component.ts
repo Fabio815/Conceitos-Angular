3a0260c24fec851222a229ecdf4622becf2ccd1e
Fabio815 <fabiomotamarques815@gmail.com>
2026-07-27 19:25:04 -0300

    Fazendo o formulário de consulta

--- src/app/consulta/consulta.component.ts
+++ src/app/consulta/consulta.component.ts
@@ -1,8 +1,23 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-consulta',
-  imports: [],
+  imports: [
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+    MatTableModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    FormsModule
+  ],
   templateUrl: './consulta.component.html',
   styleUrl: './consulta.component.scss'
 })
